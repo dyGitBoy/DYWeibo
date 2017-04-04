5b713d6010bd172d7629768a2c74f4d5b758d8ca
Yi Deng <dydyistc@gmail.com>
2017-04-04 20:47:46 +0800

    use storyboard reference

--- DYWeibo/Classes/Main(主要)/MainViewController.swift
+++ DYWeibo/Classes/Main(主要)/MainViewController.swift
@@ -12,10 +12,7 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileViewController(), title: "我", imageName: "tabbar_profile")
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,13 +20,5 @@ class MainViewController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
-    fileprivate func addChildViewController(_ childController: UIViewController, title: String, imageName : String) {
-        childController.title = title
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        
-        let childNavigationController = UINavigationController(rootViewController: childController)
-        addChildViewController(childNavigationController)
-    }
 
 }
