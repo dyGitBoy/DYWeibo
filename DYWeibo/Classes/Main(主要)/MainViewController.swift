e8a48b82ae39ce3644d7dc8269517e2a26263fc3
Yi Deng <dydyistc@gmail.com>
2017-04-05 22:21:58 +0800

    add compose button

--- DYWeibo/Classes/Main(主要)/MainViewController.swift
+++ DYWeibo/Classes/Main(主要)/MainViewController.swift
@@ -10,30 +10,19 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
-    fileprivate lazy var imageNames = ["tabbar_home", "tabbar_message_center", "", "tabbar_discover", "tabbar_profile"]
+    fileprivate lazy var composeBtn : UIButton = UIButton(imageName: "tabbar_compose_icon_add", bgImageName: "tabbar_compose_button")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupComposeBtn()
     }
     
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // for loop all items
-        for i in 0..<tabBar.items!.count {
-            
-            let item = tabBar.items![i]
-            
-            // compose is disable for now
-            if i == 2 {
-                item.isEnabled = false
-                continue
-            }
-            
-            item.selectedImage = UIImage(named: imageNames[i] + "_highlighted")
-        }
-    }
-    
+}
 
+extension MainViewController {
+    fileprivate func setupComposeBtn() {
+        tabBar.addSubview(composeBtn);
+        composeBtn.center = CGPoint.init(x: tabBar.center.x, y: tabBar.bounds.size.height * 0.5)
+    }
+    
 }
---
+++ DYWeibo/Classes/Tools/UIButton-Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton-Extension.swift
+//  DYWeibo
+//
+//  Created by Yi Deng on 05/04/2017.
+//  Copyright © 2017 TD. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    convenience init (imageName : String, bgImageName : String) {
+        self.init()
+        
+        setImage(UIImage(named: imageName), for: .normal)
+        setImage(UIImage(named: imageName + "_highlighted"), for: .highlighted)
+        setBackgroundImage(UIImage(named: bgImageName), for: .normal)
+        setBackgroundImage(UIImage(named: bgImageName + "_highlighted"), for: .highlighted)
+        sizeToFit()
+    }
+}
