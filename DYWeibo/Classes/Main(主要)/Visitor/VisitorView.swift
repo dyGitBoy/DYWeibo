de88c726ab5f2172954d550b6721b52be1f8d2b8
Yi Deng <dydyistc@gmail.com>
2017-04-08 22:52:19 +0800

    add rotation animation for home visitor page

--- DYWeibo/Classes/Main(主要)/Visitor/VisitorView.swift
+++ DYWeibo/Classes/Main(主要)/Visitor/VisitorView.swift
@@ -9,8 +9,27 @@
 import UIKit
 
 class VisitorView: UIView {
+    
+    @IBOutlet weak var rotationView: UIImageView!
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    
     class func visitorView() -> VisitorView {
         return Bundle.main.loadNibNamed("VisitorView", owner: nil, options: nil)!.first as! VisitorView
+    }
+    
+    func addRotationAnimation() {
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        
+        rotationAnimation.fromValue = 0
+        rotationAnimation.toValue = Double.pi * 2
+        rotationAnimation.repeatCount = MAXFLOAT
+        rotationAnimation.duration = 5
+        rotationAnimation.isRemovedOnCompletion = false
+        
+        rotationView.layer.add(rotationAnimation, forKey: nil)
     }
     
 }
